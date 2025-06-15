05b6ed203ad061d367be14e19eb2609b1addb6be
Bhaskar Subramanian <mooch561@yahoo.com>
2025-06-15 16:02:45 +0530

    Update tamtut.py
    got rid of absolute path reference to get the images directory.

--- tamtut.py
+++ tamtut.py
@@ -1,10 +1,11 @@
-
-
 import os
 import glob
 import random
-from PIL import ImageTk,Image
+from PIL import ImageTk, Image
 import tkinter as tk
+
+# Get the directory where this script is located, for relative paths
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 class TamLetter:
     type = 'c'
@@ -14,7 +15,7 @@ class TamLetter:
     def __init__(self, t, p):
         self.type = t
         self.pos = p
-        filename = "images" + "\\" + self.type + str(self.pos) + ".JPG"
+        filename = os.path.join(BASE_DIR, "images", f"{self.type}{self.pos}.JPG")
         self.img = ImageTk.PhotoImage(Image.open(filename).resize((200, 200)))
 
 class FlashCard:
@@ -22,14 +23,14 @@ class FlashCard:
     hrange = 0
     current = 0
     is_seq_or_rnd = 0
-    
+
     def __init__(self, l, r):
         self.lrange = l
         self.hrange = r
         self.current = self.lrange
-        
+
         print("flash card range of " + str(l) + " <-> " + str(r))
-    
+
     def next_seq(self):
         current = self.current
         print(str(current), " is the current value. is_seq_or_rnd is ", str(self.is_seq_or_rnd) )
@@ -38,75 +39,74 @@ class FlashCard:
         else:
             self.current = self.current + 1
         return(current)
-    
+
     def next_rnd(self):
         self.current = random.randint(self.lrange, self.hrange)
         return(self.current)
-    
+
     def next(self):
         if self.is_seq_or_rnd == 0:
             return(self.next_seq())
         else:
             return(self.next_rnd())
 
-
 def get_letters(type):
-    prefix = "images/" + type + "*" + ".JPG"
+    prefix = os.path.join(BASE_DIR, "images", f"{type}*.JPG")
     files = [x for x in glob.glob(prefix)]
     
     letter_objs = []
     for f in files:
-        p = f.replace("images\\", "").replace(".JPG", "").replace(type, "")
+        p = os.path.basename(f).replace(".JPG", "").replace(type, "")
         # print("adding " + str(p) + " of type " + type)
         letter_objs.append(TamLetter(type, p))
     
     return letter_objs
 
 def butCallBack():
-  global vLetters, cLetters, flashCard, is_v_or_c
-  global canvas, currimg
-  
-  canvas.delete(currimg)
-  
-  letters = cLetters
-  if is_v_or_c == 0:
-      letters = vLetters
-  
-  pos = flashCard.next(); 
-  letter = letters[0]
-  for x in letters:
-      if int(x.pos) == int(pos):
-          letter = x
-          break
-  
-  print("position: " + str(pos) + " letter here is type: " + str(letter.type) +  " pos: " + str(letter.pos) )
-  currimg = canvas.create_image(100, 100, anchor=tk.NW, image=letter.img)
-  return 1
+    global vLetters, cLetters, flashCard, is_v_or_c
+    global canvas, currimg
+
+    canvas.delete(currimg)
+
+    letters = cLetters
+    if is_v_or_c == 0:
+        letters = vLetters
+
+    pos = flashCard.next() 
+    letter = letters[0]
+    for x in letters:
+        if int(x.pos) == int(pos):
+            letter = x
+            break
+
+    print("position: " + str(pos) + " letter here is type: " + str(letter.type) +  " pos: " + str(letter.pos) )
+    currimg = canvas.create_image(100, 100, anchor=tk.NW, image=letter.img)
+    return 1
 
 def radioSelCallBack():
     global is_v_or_c, is_seq_or_rand, flashCard, vLetters, cLetters, radio_var1, radio_var2
-    
+
     if radio_var1.get() == 0:
         flashCard.hrange = len(vLetters)
         is_v_or_c = 0
     else:
         flashCard.hrange = len(cLetters)
         is_v_or_c = 1
-    
+
     flashCard.is_seq_or_rnd = radio_var2.get()
     is_seq_or_rand = radio_var2.get()
     return 1
 
 ### main ###
-os.chdir('D:\\MyCode\\python\\tamtut')
 currdir = os.getcwd()
 print("current directory is", currdir)
 
 top = tk.Tk()
 
-canvas = tk.Canvas(top, width = 400, height = 300)
+canvas = tk.Canvas(top, width=400, height=300)
 canvas.pack()
-img = ImageTk.PhotoImage(Image.open('images/header.JPG'))
+header_path = os.path.join(BASE_DIR, 'images', 'header.JPG')
+img = ImageTk.PhotoImage(Image.open(header_path))
 currimg = canvas.create_image(50, 50, anchor=tk.NW, image=img)
 
 b = tk.Button(top, text = "Next Alphabet", command = butCallBack)
@@ -134,10 +134,4 @@ r4 = tk.Radiobutton(top, text = "Random", variable = radio_var2, value = 1, comm
 r4.pack()
 r4.deselect()
 
-
-
-
-
 top.mainloop()
-
-
